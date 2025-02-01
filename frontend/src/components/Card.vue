508018ede05b8cb71eacc5a431a9488046128805
Rodrigo <rodrigo.donoso990@gmail.com>
2025-02-01 20:23:31 -0300

    Se agrega card

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,47 @@
+<script setup>
+import web from '@/components/icons/web.svg'
+import mobile from '@/components/icons/mobile.svg'
+import chatbot from '@/components/icons/chatbot.svg'
+
+</script>
+<template>
+    <div class="container mt-5">
+        <div class="row">
+
+            <div class="mt-5 col-4">
+                <h4>Desarrollo Web</h4>
+                <p>Una plataforma full-stack para la creación y gestión de sitios web, con herramientas avanzadas y
+                    optimización del rendimiento.</p>
+                <img class="container__img" :src=web alt="desarrolo web">
+            </div>
+
+            <div class="mt-5 col-4">
+                <h4>Desarrollo de app</h4>
+                <p>Una plataforma full-stack para la creación y gestión de sitios web, con herramientas avanzadas y
+                    optimización del rendimiento.</p>
+                <img class="container__img" :src=mobile alt="desarrolo web">
+            </div>
+
+            <div class="mt-5 col-4">
+                <h4>Chatbot impulsado por IA</h4>
+                <p>Chatbot inteligente para atención al cliente, que aprovecha el procesamiento del lenguaje natural.</p>
+                <img class="container__img" :src=chatbot alt="desarrolo web">
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container__img {
+    width: 50%;
+}
+
+h4 {
+    font-family: "Orbitron";
+    color: white;
+}
+p{
+    color: #7f8ea3;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,32 @@
 <script setup>
 import Carousel from '@/components/Carousel.vue';
-
+import Card from '@/components/Card.vue';
 </script>
 
 <template>
-    <Carousel />
 
-    <h1>vista Home</h1>
+    <Carousel />
+    <main>
+
+        <h1 class="text-center pt-5">Algunos servicios</h1>
+        
+        <section>
+            <Card />
+        </section>
+
+    </main>
+    
 </template>
+
+
+<style scoped>
+main {
+    background-color: #0f1629;
+}
+
+h1 {
+    color: white;
+    font-family: "Orbitron";
+
+}
+</style>
